eeac126fa1c582ca59a2fda9cbc08682f5d5fc01
w0428561 <w0428561@nscc.ca>
2021-02-28 14:54:29 -0400

    completed file

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+  
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  $(document).ready(function() {
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+    
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+
